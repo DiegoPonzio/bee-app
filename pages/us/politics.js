5b82b0bc6e914cbc58cdc95a804f4c17d5e2833a
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-24 15:45:53 -0600

    politicas

---
+++ pages/us/politics.js
@@ -0,0 +1,19 @@
+import Layout from "../../components/Layout"
+import Poli from "../../components/Politicas"
+import NavBar from "../../components/NavBar"
+import Footer from "../../components/Footer"
+
+export default function Politicas () {
+    return (
+        <>
+            <Layout title="Políticas" />
+            <NavBar />
+            <div className="py-10 px-10">
+                <Poli />
+                <Footer />
+            </div>
+        </>
+
+    )
+}
+
