40befe0c2ee10d00b5dfb83b52401a3ee60aede6
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-24 15:40:59 -0600

    gg

---
+++ clientServices/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+      id: 1,
+      title: 'Mision',
+      img:'https://cdn-icons-png.flaticon.com/512/81/81752.png',
+      random:
+        'Ser el principal medio para la difusión de la información de las instituciones académicas del IPN a nivel Medio Superior.',
+    },
+    {
+      id: 2,
+      title: 'Vision',
+      img: 'https://cdn-icons-png.flaticon.com/512/4389/4389297.png',
+      random: 'Ser la herramienta principal en la consulta de información de toda la comunidad estudiantil y para los estudiantes que cada año se van integrando. A su vez, ser el medio de conexión para la obtención de información y respuesta a las propuestas que realizan instituciones externas al IPN sobre la realización de algún evento o actividad a favor de la comunidad. ',
+    },
+    {
+      id: 3,
+      title: 'Filosofia',
+      img: 'https://cdn-icons-png.flaticon.com/512/2421/2421033.png',
+      random:
+        'Creemos que una conexión entre nuestros compañeros y comunidad politécnica contribuye a la participación, conversación e identidad que promueve un futuro donde parte de la desinformación o mala distribución de la misma pueda generar conflictos o menos oportunidades para nuestra comunidad.',
+    },
+  ];
+  
+  export default data;
