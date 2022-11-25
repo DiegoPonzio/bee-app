1311877270e672639449d0b9fc03fe9ca42e1344
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-25 01:14:35 -0600

    redirect

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -2,5 +2,5 @@ import withSession from "../../lib/session";
 
 export default withSession(async (req, res) => {
     req.session.destroy();
-    res.redirect('/', 302)
+    res.redirect('/login')
 })
