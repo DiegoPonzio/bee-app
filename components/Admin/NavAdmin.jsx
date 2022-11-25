3c8375fd0734d8e4502e912a81534be3ca72b39f
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-25 16:38:02 -0600

    logout

--- components/Admin/NavAdmin.jsx
+++ components/Admin/NavAdmin.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { BiUserCircle } from "react-icons/bi";
 import Link from "next/link";
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {AiOutlineClose} from 'react-icons/ai'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineClose } from 'react-icons/ai'
+import axios from 'axios'
+import Router from "next/router";
 
 export default function NavAdmin({ type }) {
   const [status, setStatus] = useState(false)
@@ -40,7 +42,7 @@ export default function NavAdmin({ type }) {
     <nav className={`bg-amber-300 z-50 ${status && "fixed w-full"}`}>
       <div className="flex items-center font-medium justify-around">
         <div className="z-50 p-5 md:w-auto w-full flex justify-between">
-        <h2 className="text-7xl cursor-pointer"><Link href={"/user/home"} legacyBehavior>BEE+</Link></h2>
+          <h2 className="text-7xl cursor-pointer"><Link href={"/user/home"} legacyBehavior>BEE+</Link></h2>
           <div className="text-3xl md:hidden" onClick={() => setOpen(!open)}>
             {open ? <AiOutlineClose /> : <GiHamburgerMenu />}
           </div>
@@ -61,12 +63,10 @@ export default function NavAdmin({ type }) {
         <ul className={`md:hidden bg-amber-300 absolute w-full h-full bottom-0 py-24 pl-4 duration-500 z-20 ${open ? 'left-0' : 'left-[-100%]'}`}>
           <li><Link href="/user/admin/Solicitudes" legacyBehavior><a className="text-black  hover:text-yellow-200">Solicitudes</a></Link></li>
           <li><Link href="/user/home" legacyBehavior><a className="text-black  hover:text-yellow-200">Publicaciones</a></Link></li>
-          <div className="md:hidden flex uppercase items-end gap-8">
-          <Link href="/api/logout" legacyBehavior><a className="text-black  hover:text-yellow-200 ">
+          <div className="md:hidden flex uppercase items-end gap-8" onClick={ () => axios.get('/api/logout').then( () => Router.replace('/') )}>
             <BiUserCircle size={50} />
             <span className="sr-only">Usuario</span>
-          </a></Link>
-        </div>
+          </div>
         </ul>
       </div>
     </nav>
--- pages/api/logout.js
+++ pages/api/logout.js
@@ -2,7 +2,7 @@ import withSession from "../../lib/session";
 
 export default withSession(async (req, res) => {
     req.session.destroy();
-    res.setHeader("location", "/")
-    res.statusCode = 302
-    res.end()
+    res.status(200).json({
+        res: "OK"
+    })
 })
