22fc1aaab303ac19fb7c2b6daf5df4e9529b472f
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-25 01:31:33 -0600

    redirect

--- components/Navbar/MyButton.jsx
+++ components/Navbar/MyButton.jsx
@@ -30,8 +30,8 @@ export default function Mybutton() {
                                     <li className='text-sm text-gray-600 my-2.5'>
                                         <Link href={"/signout"} legacyBehavior><a className='hover:text-orange-900'>Registarse</a></Link>
                                     </li>
-                                    <li className='text-sm text-gray-600 my-2.5'>
-                                        <Link href={"/api/outCecyt"} legacyBehavior><a className='hover:text-orange-900' onClick={ () => window.location.reload()}>Elejir CECyT</a></Link>
+                                    <li className='text-sm text-gray-600 my-2.5' onClick={ () => window.location.reload()}>
+                                        <Link href={"/api/outCecyt"} legacyBehavior><a className='hover:text-orange-900'>Elejir CECyT</a></Link>
                                     </li>
                                 </ul>
                             </div>
