be7e9f39cecdecefbd5f9407c9e05ea736c60815
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-25 15:11:50 -0600

    redirect

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,12 +1,6 @@
 import withSession from "../../lib/session";
 
 export default withSession(async (req, res) => {
-    switch (req.method) {
-        case "GET":
-            req.session.destroy();
-            res.redirect('/')
-
-        default:
-            return res.status(400).json({ message: "Bad request", status: 400 })
-    }
+    req.session.destroy();
+    res.redirect('/', 308)
 })
