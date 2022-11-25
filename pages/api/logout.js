3320cb7e01543650f9d7359efef0032faa6f30f7
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-25 17:09:34 -0600

    logout

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -2,5 +2,5 @@ import withSession from "../../lib/session";
 
 export default withSession((req, res) => {
     req.session.destroy();
-    res.redirect(308,'/')
+    res.redirect(308,'https://bee-pruebas.herokuapp.com/')
 })
