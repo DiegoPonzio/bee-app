3b05ff508f5dbb4782ff658564a60fec97e298cf
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-25 00:28:56 -0600

    posts

---
+++ components/Cards.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import {FaHeart} from 'react-icons/fa'
+import { FiHeart } from 'react-icons/fi'
+
+export default function Cards({ img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZhLHBfMTrKT4HCY7Lyue8ul7R_G5S24zHBT73LSjA2Fi536zNOPBM33V3SbVsSzaY3Uc&usqp=CAU", name, body, date, hour, place }) {
+
+    const [like, setLike] = useState(false)
+
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg md:cursor-pointer mx-30px mt-4">
+            <img className="w-full" src={`${img}`} alt={name} onDoubleClick={() => setLike(!like)}  />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2 ">{name}<div className='float-right mt-1' onClick={() => setLike(!like)} >{like ? <FaHeart size={25} color="red" /> : <FiHeart size={25} color="grey" />}</div></div> 
+                <p className="text-gray-700 text-base">
+                    {body}
+                </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Fecha: {date}</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Hora: {hour}</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Lugar: {place}</span>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,24 @@ import NavBar from "../components/NavBar"
 import Footer from "../components/Footer"
 import withSessionCecyt from "../lib/cecyt"
 import Slider1 from "../components/Slider1"
+import { useState } from "react"
+import Spinners from "../components/Spinners"
 
 export default function Home({ cecyt }) {
   const { carrear, name } = cecyt
+  const [posts, setPosts] = useState()
+  const [error, setError] = useState()
+  const URL = `https://bee-pruebas.herokuapp.com/api/showAll/byCecyt/${name}`
+
+  const fetchPosts = async () => {
+    const response = await fetch(URL)
+      .then(response => response.json())
+      .then(responseJSON => {
+        setError(false)
+        setPosts(responseJSON)
+      })
+      .catch(() => setError(true))
+  }
 
   return (
     <>
@@ -14,19 +29,24 @@ export default function Home({ cecyt }) {
       <div className='py-5 px-10'>
         <Slider1 />
         <div className="grid gird-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 items-center justify-items-center">
-          {/*!posts && <Spinners />*/}
-          {/*posts && posts.result && posts.result.map(post => {
+          {!posts && !error && <Spinners />}
+          {posts && !error && posts.result && posts.result.map(post => {
             //console.log(bufferToBinaryString(post.pub_media.data))
-            return <Cards img={Buffer.from(post.pub_media).toString('base64')} name={post.pub_titulo} body={post.pub_descripcion} date={post.pub_fecha} hour={post.pub_horainicio} place={post.pub_locacion} key={post.pub_id} />
-          })*/}
+            return <Cards name={post.pub_titulo} body={post.pub_descripcion} date={post.pub_fecha} hour={post.pub_horainicio} place={post.pub_locacion} key={post.pub_id} />
+          })}
         </div>
-        {/*posts && (
+        {error && !posts && (
+          <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg" role="alert">
+            <span className="font-medium">Lo sentimos!!</span> No encontramos comunicados  
+          </div>
+        )}
+        {posts && (
           <>
             <br />
             <br />
             <br />
           </>
-        )*/}
+        )}
         <Footer />
       </div>
     </>
