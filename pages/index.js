0bf7910a6ef887f97830d15621482ea3de597115
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-11-26 22:34:36 -0600

    error

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ cecyt }) {
         //setError(false)
         setPosts(responseJSON)
       })
-      .catch(() => setError(true))
+      .catch(console.log)
   }
 
   useEffect( () => {
