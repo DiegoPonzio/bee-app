66d1002a57c4fbdbaf6472f81620222bc29d133a
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-12-02 23:50:58 -0600

    br

--- components/OtherUsers/PostComment.jsx
+++ components/OtherUsers/PostComment.jsx
@@ -12,7 +12,7 @@ export default function PostComment() {
                     <input type="text" className={`block p-2.5 w-full text-sm text-gray-900 bg-amber-50 hover:bg-yellow-200 rounded-lg border border-gray-300 focus:ring-yellow-500 focus:bg-yellow-300 focus:border-amber-500 ${errors.nombre && 'border-red-500'} `} placeholder="Bee Assistant" {...register("nombre", { required: true, pattern: /^[a-zA-Z\u00C0-\u017f\s]+?[0-9]{0,8}$/ })} />
                     {errors.nombre && <p className="text-red-500 text-xs italic">El nombre no es válido</p>}
                 </div>
-
+                <br/>
                 <div>
                     <label forHTML="message" className="block mb-2 text-sm font-medium text-gray-900">Escribe tu mensaje: </label>
                     <textarea id="message" rows="4" className={`block p-2.5 w-full text-sm text-gray-900 bg-amber-50 hover:bg-yellow-200 rounded-lg border border-gray-300 focus:ring-yellow-500 focus:bg-yellow-300 focus:border-amber-500 ${errors.comentario && 'border-red-500'} `} placeholder="Escribe tus comentarios aquí..." {...register("comentario", { required: true, pattern: /^[a-zA-Z\u00C0-\u017f\s]+?[0-9]{0,8}$/ })} ></textarea>
