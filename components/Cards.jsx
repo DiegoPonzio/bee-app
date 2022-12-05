e8c0fdda70a0cc838b54621b084e7100ff12a359
DiegoPonzio <druizp2000@alumno.ipn.mx>
2022-12-04 22:35:43 -0600

    comentario

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -46,12 +46,13 @@ export default function Cards({ img = "https://encrypted-tbn0.gstatic.com/images
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Hora: {hour}</span>
                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Lugar: {place}</span>
             </div>
-            <div className='px-6 pt-4 pb-2'>
-                <a href={link} target="_blank" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-yellow-200 hover:bg-yellow-300 rounded-lg focus:border-amber-500">
+            <div className='px-6 pt-4 pb-2 justify-center items-center'>
+                <a href={link} target="_blank" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-amber-300 hover:bg-yellow-200 rounded-lg focus:border-amber-500">
                     Leer más...
                     <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
             </div>
+            <br/>
             <div className='rounded grid grid-cols-1 place-items-center gap-1'>
                 <div onClick={() => setComment(!comment)}>
                     <div className='flex justify-between'>
--- components/OtherUsers/PostComment.jsx
+++ components/OtherUsers/PostComment.jsx
@@ -39,7 +39,7 @@ export default function PostComment({ id }) {
 
                 <br />
 
-                <button type="submit" className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-yellow-200 hover:bg-yellow-300 rounded-lg hover:bg-yellow-300 focus:border-amber-500 ">
+                <button type="submit" className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-yellow-300 hover:bg-yellow-20 rounded-lg focus:border-amber-500 ">
                     Publicar Comentario
                 </button>
 
