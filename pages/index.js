d7454a9372329f9c7ae893f976da0b43ec09ca15
DiegoPonzio <druizp2000@alumno.ipn.mx>
2023-02-04 21:32:04 -0600

    server-side-exeption

--- pages/index.js
+++ pages/index.js
@@ -8,54 +8,60 @@ import Spinners from "../components/Spinners"
 import { NotificationContainer } from "react-notifications"
 import axios from "axios"
 import Cards from "../components/Cards"
+import Router from "next/router"
 
 export default function Home({ cecyt }) {
-  const { carrear, name } = cecyt
-  const [posts, setPosts] = useState()
-  const [error, setError] = useState(false)
-  const URL = `/api/showAll/byCecyt/${name}`
 
-  const fetchPosts = async () => {
-    const response = await axios.get(URL)
-      .then(setPosts)
-      .catch(() => setError(true))
-  }
+  try {
 
-  useEffect( () => {
-    !posts && fetchPosts()
-  }, [posts])
+    const { carrear, name } = cecyt
+    const [posts, setPosts] = useState()
+    const [error, setError] = useState(false)
+    const URL = `/api/showAll/byCecyt/${name}`
 
-  return (
-    <>
-      {!cecyt && Router.replace("/")}
-      <Layout title={name} />
-      <NavBar carrear={carrear} />
-      <NotificationContainer />
-      <div className='py-5 px-10'>
-        <Slider1 />
-        <div className="grid gird-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 items-center justify-items-center">
-          {!posts && !error && <Spinners />}
-          {posts && !error && posts.data.result && posts.data.result.map(post => {
-            //console.log(bufferToBinaryString(post.pub_media.data))
-            return <Cards img={String.fromCharCode(...post.pub_media.data)} name={post.pub_titulo} body={post.pub_descripcion} date={post.pub_fecha} hour={post.pub_horainicio} hour2={post.pub_horafinal} place={post.pub_locacion} key={post.pub_id} id={post.pub_id} link={post.pub_fuente} who={post.pub_encargado} />
-          })}
-        </div>
-        {error && (
-          <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg" role="alert">
-            <span className="font-medium">Lo sentimos😿!!</span> No se encontrarón comunicados en este apartado.  
+    const fetchPosts = async () => {
+      const response = await axios.get(URL)
+        .then(setPosts)
+        .catch(() => setError(true))
+    }
+
+    useEffect(() => {
+      !posts && fetchPosts()
+    }, [posts])
+
+    return (
+      <>
+        <Layout title={name} />
+        <NavBar carrear={carrear} />
+        <NotificationContainer />
+        <div className='py-5 px-10'>
+          <Slider1 />
+          <div className="grid gird-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 items-center justify-items-center">
+            {!posts && !error && <Spinners />}
+            {posts && !error && posts.data.result && posts.data.result.map(post => {
+              //console.log(bufferToBinaryString(post.pub_media.data))
+              return <Cards img={String.fromCharCode(...post.pub_media.data)} name={post.pub_titulo} body={post.pub_descripcion} date={post.pub_fecha} hour={post.pub_horainicio} hour2={post.pub_horafinal} place={post.pub_locacion} key={post.pub_id} id={post.pub_id} link={post.pub_fuente} who={post.pub_encargado} />
+            })}
           </div>
-        )}
-        {posts &&  !error && (
-          <>
-            <br />
-            <br />
-            <br />
-          </>
-        )}
-        <Footer />
-      </div>
-    </>
-  )
+          {error && (
+            <div className="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg" role="alert">
+              <span className="font-medium">Lo sentimos😿!!</span> No se encontrarón comunicados en este apartado.
+            </div>
+          )}
+          {posts && !error && (
+            <>
+              <br />
+              <br />
+              <br />
+            </>
+          )}
+          <Footer />
+        </div>
+      </>
+    )
+  } catch (error) {
+    Router.replace("/principal")
+  }
 }
 
 export const getServerSideProps = withSessionCecyt(async function ({ req, res }) {
