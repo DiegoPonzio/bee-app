f53631055a85b7b2a058da4503ef9118ae9aac9c
DiegoPonzio <druizp2000@alumno.ipn.mx>
2023-02-07 12:08:24 -0600

    edit fecha

--- components/Admin/Editar.jsx
+++ components/Admin/Editar.jsx
@@ -117,7 +117,8 @@ export default function Editar({ id, user }) {
                     </div>
                     <div>
                       <label
-                        htmlFor="phone"
+                        htmlFor="hasta"
+                        defaultValue= {hoyString.split(re)[0]}
                         className="block mb-2 text-sm font-medium text-gray-900">Hasta: </label>
                       <input type="datetime-local" id="hasta"
                         className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${errors.hasta && 'border-red-500'}`}
