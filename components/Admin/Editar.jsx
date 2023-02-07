703b2fe0f3334829ea8f068c2f5842183c03e8dc
DiegoPonzio <druizp2000@alumno.ipn.mx>
2023-02-07 12:11:01 -0600

    error

--- components/Admin/Editar.jsx
+++ components/Admin/Editar.jsx
@@ -118,7 +118,7 @@ export default function Editar({ id, user }) {
                     <div>
                       <label
                         htmlFor="hasta"
-                        defaultValue= {hoyString.split(re)[0]}
+                        
                         className="block mb-2 text-sm font-medium text-gray-900">Hasta: </label>
                       <input type="datetime-local" id="hasta"
                         className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${errors.hasta && 'border-red-500'}`}
