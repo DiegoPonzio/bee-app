e4b42db479ad227d5dc08f3358558ff8351d0007
DiegoPonzio <druizp2000@alumno.ipn.mx>
2023-03-19 15:08:18 -0600

    Style cecyts

--- components/OtherUsers/Seleccionar.jsx
+++ components/OtherUsers/Seleccionar.jsx
@@ -3,7 +3,7 @@ import Cecyt from '../../components/OtherUsers/Cecyt'
 
 export default function Seleccionar() {
     return (
-        <div className="grid grid-cols-1 gap-4 p-16 lg:grid-cols-5">
+        <div className="grid grid-cols-1 gap-4 p-16 lg:grid-cols-5 pt-28">
             {cecyts.map((cecyt) => (
                 <Cecyt src={cecyt.img} name={cecyt.name} key={cecyt.id} carrear={cecyt.area.DeCarrera} />
             ))}
